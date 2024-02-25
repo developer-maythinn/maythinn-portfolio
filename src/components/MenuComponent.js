ace75b2d6fbfe649073a2a2155b8ecada950dfab
maythinn <90564038+developer-maythinn@users.noreply.github.com>
2024-02-25 18:30:16 +0630

    scroll block and custom check

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,55 +1,88 @@
-import { Input } from "@mui/material";
-import Link from "next/link";
+import { Box, Checkbox, Fade, Input } from "@mui/material";
+import { useRouter } from "next/navigation";
 import React from "react";
 import { pages } from "./dummyData";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import useScrollBlock from "./useScrollBlock";
 
 function MenuComponent() {
-  return (
-    <div className="navigation">
-      <input
-        type="checkbox"
-        className="navigation__checkbox"
-        id="navi-toggle"
-      />
-      <label for="navi-toggle" className="navigation__button">
-        <span className="navigation__icon">&nbsp;</span>
-      </label>
-      <div className="navigation__background">&nbsp;</div>
-      <nav className="navigation__nav">
-        <ul className="navigation__list">
-          {pages.map((page, index) => (
-            <React.Fragment key={index}>
-              <li className="navigation__item">
-                <Link href={page.url} className="navigation__link">
-                  {page.title}
-                </Link>
-              </li>
-            </React.Fragment>
-          ))}
+  const [checked, setChecked] = React.useState(false);
 
-          {/* <li className="navigation__item">
-            <a href="#" className="navigation__link">
-              About us
-            </a>{" "}
-          </li>
-          <li className="navigation__item">
-            <a href="#" className="navigation__link">
-              Careers
-            </a>{" "}
-          </li>
-          <li className="navigation__item">
-            <a href="#" className="navigation__link">
-              Projects
-            </a>{" "}
-          </li>
-          <li className="navigation__item">
-            <a href="#" className="navigation__link">
-              Contact Us
-            </a>{" "}
-          </li> */}
-        </ul>
-      </nav>
-    </div>
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+  console.log(checked);
+  const router = useRouter();
+  const handleClick = (e, href) => {
+    e.preventDefault();
+    setChecked(false);
+    router.push(href);
+  };
+  const [blockScroll, allowScroll] = useScrollBlock();
+  if (checked) {
+    blockScroll(false);
+  } else {
+    allowScroll(true);
+  }
+
+  return (
+    <>
+      <Checkbox
+        checked={checked}
+        onChange={handleChange}
+        inputProps={{ "aria-label": "controlled" }}
+        sx={{
+          position: "relative",
+          zIndex: 9999,
+          "&.Mui-checked": {
+            transition: "2s",
+          },
+        }}
+        checkedIcon={<CloseIcon />}
+        icon={<MenuIcon />}
+      />
+      <div className="navigation">
+        <input
+          type="checkbox"
+          className="navigation__checkbox"
+          id="navi-toggle"
+        />
+        <label for="navi-toggle" className="navigation__button">
+          <span className="navigation__icon">&nbsp;</span>
+        </label>
+        <Box
+          className="navigation__background"
+          sx={{ transform: checked ? "scale(90)" : "scale(0)" }}
+        >
+          &nbsp;
+        </Box>
+        <Box
+          component={"nav"}
+          className="navigation__nav"
+          sx={{
+            opacity: checked ? 1 : 0,
+            width: checked ? "100%" : "0",
+            zIndex: 1500,
+          }}
+        >
+          <ul className="navigation__list">
+            {pages.map((page, index) => (
+              <React.Fragment key={index}>
+                <li className="navigation__item">
+                  <a
+                    onClick={(e) => handleClick(e, page.url)}
+                    className="navigation__link"
+                  >
+                    {page.title}
+                  </a>
+                </li>
+              </React.Fragment>
+            ))}
+          </ul>
+        </Box>
+      </div>
+    </>
   );
 }
 
